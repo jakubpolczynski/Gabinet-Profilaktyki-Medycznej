dc03db7cb2a39f420d67434b97e45d47e8fedc7c
GL65_LEOPARD\jakub <jakub.polczynski00@gmail.com>
2022-11-08 21:32:50 +0100

    Forms first look and project documentation

--- Gabinet Profilaktyki Medycznej/Gabinet Profilaktyki Medycznej/Form1.Designer.cs
+++ Gabinet Profilaktyki Medycznej/Gabinet Profilaktyki Medycznej/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_imie = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label_imie
+            // 
+            this.label_imie.AutoSize = true;
+            this.label_imie.Location = new System.Drawing.Point(12, 9);
+            this.label_imie.Name = "label_imie";
+            this.label_imie.Size = new System.Drawing.Size(26, 13);
+            this.label_imie.TabIndex = 2;
+            this.label_imie.Text = "Imie";
+            this.label_imie.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label_imie.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(549, 625);
+            this.Controls.Add(this.label_imie);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Gabinet Profilaktyki Medycznej";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_imie;
     }
 }
 
--- Gabinet Profilaktyki Medycznej/Gabinet Profilaktyki Medycznej/Form1.cs
+++ Gabinet Profilaktyki Medycznej/Gabinet Profilaktyki Medycznej/Form1.cs
@@ -16,5 +16,20 @@ namespace Gabinet_Profilaktyki_Medycznej
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
