9e81cd3cfcf8f4249e41793970ecc5588b3a7509
os49359 <117598313+olejs10@users.noreply.github.com>
2023-05-24 19:23:49 +0200

    obsluga inputow do archiwizacji

--- Gabinet Profilaktyki Medycznej/scripts/archive_examination.js
+++ Gabinet Profilaktyki Medycznej/scripts/archive_examination.js
@@ -1,12 +1,10 @@
-async function printExaminations() {
-    const dateL = document.getElementById("print_examination_dateL").value;
-    const dateR = document.getElementById("print_examination_dateR").value;
-    const stClass = document.getElementById("print_examination_class").value;
-    const fname = document.getElementById("print_examination_fname").value;
-    const lname = document.getElementById("print_examination_lname").value;
+async function archiveExaminations() {
+    const dateL = document.getElementById("archive_examination_dateL").value;
+    const dateR = document.getElementById("archive_examination_dateR").value;
+    const type = document.querySelector('input[name="examination_type"]:checked').value;
 
     // sprawdź, czy wszystkie pola formularza są wypełnione
-    if (dateL || dateR || stClass || fname || lname) 
+    if (dateL || dateR) 
     {
         var ecc = document.getElementById("examination-content-container");
             while (ecc.hasChildNodes()) {
@@ -21,7 +19,7 @@ async function printExaminations() {
         // Ustawiamy nagłówki
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         // Przygotowanie danych
-        const data = `dateL=${dateL}&dateR=${dateR}&stClass=${stClass}&fname=${fname}&lname=${lname}`;
+        const data = `dateL=${dateL}&dateR=${dateR}&type=${type}`;
         // Wysłanie żądania
         dataAlreadySent = false;
         if(!dataAlreadySent){
@@ -33,6 +31,27 @@ async function printExaminations() {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     console.log("Archiwizacja przebiegła pomyślnie")
+                    var response = xhr.responseText;
+                    events = cutEvent(response);
+                    var ecc = document.getElementById("examination-content-container");
+                    for (let items of events)
+                    {
+                        var examination_content = document.createElement("div")
+                        examination_content.id = "examination-content";
+                        arr = cutParts(items);
+                        for (let item of arr)
+                        {
+
+    
+                            var examination_paragraph = document.createElement("p");
+                            examination_paragraph.id = "examination_p_content";
+                            examination_paragraph.innerText = item;
+    
+                            examination_content.appendChild(examination_paragraph)
+                            ecc.appendChild(examination_content);
+                        }
+                           
+                    }
                 } 
                 else {
                     errormsg += xhr.responseText;
